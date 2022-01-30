48fe5c163e32d96eda484024d9bdc0bbd72edcba
carlos diaz <diazmelian.carlos@gmail.com>
2022-01-30 14:36:58 +0000

    single page webapp exercice part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, Breadcrumb, BreadcrumbItem
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const RenderDish = ({ dish }) => {
   if (dish != null) {
@@ -54,9 +58,21 @@ const RenderComments = (comments) => {
 const DishDetail = (props) => {
   if (props.dish != null) {
     return (
-      <div className='row'>
-        <RenderDish dish={props.dish} />
-        <RenderComments comments={props.dish.comments} />
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <RenderDish dish={props.dish} />
+          <RenderComments comments={props.comments} />
+        </div>
       </div>
     );
   } else {
